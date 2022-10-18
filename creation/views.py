5e8f9c3996a3d8b581a2a39d811a1e4e203998b7
unknown <baws@cin.ufpe.br>
2022-10-18 13:17:23 -0300

    Integração com servidor

--- creation/views.py
+++ creation/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from django.views.generic import View
+from django.http import QueryDict
 
 
 class index(View):
@@ -17,4 +18,15 @@ class index(View):
         template = loader.get_template('creation/interface.html')
         context = {'resultado': 1}
         return HttpResponse(template.render(context, request))
+    
+    def post(self, request, *args, **kwargs):
+        artista1 = request.POST.get('artist1')
+        artista2 = request.POST.get('artist2')
+        mistura = request.POST.get('mistura')
+
+        data = {}
+        template_name = 'creation/result.html'
+        context = {'resultado':artista2}
+        data['result'] = render_to_string(template_name, context, request=request)
+        return JsonResponse(data)
 
